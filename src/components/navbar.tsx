f13125dc4a0d5c66bcad2bcf05a5b026075ef8e7
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-05-31 22:54:03 +0700

    web

---
+++ src/components/navbar.tsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+    return (
+    
+            <div className="h-[70px] w-full fixed flex items-center">
+                <p className="ml-20 text-3xl font-bold">LUTH</p>
+                <div className="flex justify-end w-full">
+                <div className="flex justify-evenly w-5/12 text-center items-center mr-20">
+                    <p className=" text-xl font-semibold">Home</p>
+                    <p className=" text-xl font-semibold">About</p>
+                    <p className=" text-xl font-semibold">Portofolio</p>
+                    <p className=" text-xl font-semibold">Contact</p>
+                </div>
+                </div>
+               
+            </div>
+
+    )
+}
